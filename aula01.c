36aa92942badbc553c8b4d3a2f4b13fc450139f0
aurogabriel <105260420+aurogabriel@users.noreply.github.com>
2022-08-29 08:55:50 -0300

    Update aula01.c

--- aula01.c
+++ aula01.c
@@ -1,3 +1,15 @@
+/*########################################################
+ * Título:
+ * !!!!!!!!!!!!!! Estudos da DSP- Pisca LED !!!!!!!!!!!!!
+ *
+ * Autor: Auro Gabriel - 08/2022
+ *
+ * Mapa das portas usadas:
+ *
+ *      GPIO0 - Pino 40 => 10 do J4
+ *
+ * #######################################################
+ */
 
 #include "F28x_Project.h"
 Uint32 i;
@@ -6,7 +18,7 @@ int main(void)
 {
     InitSysCtrl();
     EALLOW; //Registradores protegidos (Não é o caso do GPIO0 mas coloquei pra exemplificar o uso)
-    GpioCtrlRegs.GPAGMUX1.bit.GPIO0=0;  //Configuro o MUX da porta
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO0=0;  //Configuro o grupo de MUX da porta
     GpioCtrlRegs.GPAMUX1.bit.GPIO0=0; //Configuro o MUX da porta
     GpioCtrlRegs.GPADIR.bit.GPIO0=1;   //Configuro Se é input ou output
     GpioCtrlRegs.GPAPUD.bit.GPIO0=1;   //Configuro se o pull up ta ligado ou desligado PUD= PullUp Disable
@@ -15,8 +27,7 @@ int main(void)
     GpioDataRegs.GPADAT.bit.GPIO0=1;
     while(1){
             for(i=0;i<0x000FFFFF;i++){} //Delay que faz ele piscar -- Uma contagem muito grande que não faz nada
-            GpioDataRegs.GPATOGGLE.bit.GPIO0=1;
-
+            GpioDataRegs.GPATOGGLE.bit.GPIO0=1; // GPATOGGLE -> Regs que faz o Toggle.
         }
-
+    return;
 }
